6f87730665a51739edcd81acc856c7742854977c
Shilpa <shilpa@zysk.in>
2020-07-13 12:05:56 +0530

    Removed comment and installed npm dependencies

--- comps/Blog/Tag/index.js
+++ comps/Blog/Tag/index.js
@@ -2,28 +2,6 @@ import Tag from './tag';
 import styles from '../blog.module.scss';
 
 function Tags(props) {
-  // const [selectedTag, setSelectedTag] = useState(props.tags[0]);
-  // const [tagBlogs, setTagBlogs] = useState([]);
-
-  // to filter blogs according to selected tag
-  // useEffect(() => {
-  //   let tag = selectedTag,
-  //     filteredArray = [];
-
-  //   // to calculate no of blogs under selected tag
-  //   props.blogs.map((blog, i) => {
-  //     if (blog.categories.indexOf(tag.name) > -1) {
-  //       filteredArray.push(blog);
-  //     }
-  //   });
-
-  //   setTagBlogs(filteredArray);
-  // }, [selectedTag]);
-
-  // const scrollToAll = () => {
-  //   var elmnt = document.getElementById('all-blogs');
-  //   elmnt.scrollIntoView();
-  // };
   return (
     <div className="mt-5">
       <div className={`pt-5 d-flex flex-wrap mt-5 ${styles.tagList}`}>
