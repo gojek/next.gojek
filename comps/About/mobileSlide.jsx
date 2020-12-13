3bb71155c9ef7ba97fdaf7798ce2dc31efe24bd3
varunumesh <varun@zysk.in>
2020-12-13 15:39:28 +0530

    Feat: About page - Added all svg images and bgs - Created 2 components

---
+++ comps/About/mobileSlide.jsx
@@ -0,0 +1,19 @@
+function Slide(props) {
+  const { data } = props;
+  const anchor = <a className="text-green-light link" href={data.linkURL}>{data.linkText}</a>;
+
+  return (
+    <div
+      className={`${data.bgClass} ${data.bgPattern} card align-items-center justify-content-around p-4`}
+    >
+      <div class="card-body flex-grow-0">
+        <h2 className="heading">{data.title}</h2>
+        <p class="card-text">{data.description}</p>
+        { data.linkText ? anchor : '' }
+      </div>
+      <img src={data.img} class="card-img-bottom img-fluid illustration" alt={data.title} />
+    </div>
+  );
+}
+
+export default Slide;
