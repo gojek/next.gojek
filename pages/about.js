ee6874f9a741a132d2271bee6d93a5fe165a11fb
varunumesh <varun@zysk.in>
2021-01-03 23:59:04 +0530

    Feat: About us indicators - Custom indicators for desktop screens

--- pages/about.js
+++ pages/about.js
@@ -1,18 +1,5 @@
 import Navbar from '~/../../comps/Navbar';
 import Milestones from '../comps/About/milestone';
-import TechFacts from '../comps/Home/techFacts';
-import Projects from '../comps/Home/OpenSource';
-import Link from '../comps/Common/link';
-import CommonCta from '~/../../comps/Common/Cta';
-import Funding from '../comps/Home/funding';
-import Slider from 'react-slick';
-
-const sliderSettings = {
-  infinite: false,
-  dots: false,
-  slidesToShow: 1.2,
-  slidesToScroll: 1,
-};
 
 function About() {
   return (
