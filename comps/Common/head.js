f412cfa89fd69c9ebf063a2942041c949a5fa5e9
Praful Raj H M <praful.raj@zysk.in>
2021-02-22 11:06:42 +0530

    fix - og:description

--- comps/Common/head.js
+++ comps/Common/head.js
@@ -6,40 +6,18 @@ function PageMeta(props) {
     <div>
       <Head>
         <title>{props.title}</title>
-        <meta
-          property="og:title"
-          content={`${props.title}`}
-          key="title"
-        />
         <meta content="yes" name="apple-mobile-web-app-capable" />
-        <meta
-          property={`description`}
-          content={`${props.description}`}
-        />
-        <meta
-          property="twitter:title"
-          content={`${props.title}`}
-        />
-        <meta
-          property={`twitter:card`}
-          content={`${props.title}`}
-        />
+        <meta property={`description`} content={`${props.description}`} />
+        <meta property="twitter:title" content={`${props.title}`} />
+        <meta property={`twitter:card`} content={`${props.title}`} />
         <meta property={`twitter:site`} content="@gojektech" />
         <meta property={`twitter:image`} content="/img/og-img.jpg" />
-        <meta
-          property="twitter:description"
-          content={`${props.title}`}
-        />
-        <meta
-          property="og:title"
-          content={`${props.title}`}
-        />
+        <meta property="twitter:description" content={`${props.title}`} />
+        <meta property="og:title" content={`${props.title}`} key="title" />
+        {/* <meta property="og:title" content={`${props.title}`} /> */}
         <meta property="og:url" content="https://gojek.io" />
         <meta property="og:image" content="/img/og-img.jpg" />
-        <meta
-          property="og:description"
-          content={`${props.title}`}
-        />
+        <meta property="og:description" content={`${props.description}`} />
         <meta property="og:type" content={'website'} />
         <link rel="shortcut icon" href="/img/favicon.png" type="image/x-icon " />
         <link rel="icon" href="/img/favicon.png" type="image/x-icon" />
