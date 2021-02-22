96de88d19d766e901b6b5baae7030174e280e424
Praful Raj H M <praful.raj@zysk.in>
2021-02-22 14:18:23 +0530

    fix - description meta tag

--- comps/Common/head.js
+++ comps/Common/head.js
@@ -7,17 +7,16 @@ function PageMeta(props) {
       <Head>
         <title>{props.title}</title>
         <meta content="yes" name="apple-mobile-web-app-capable" />
-        <meta property={`description`} content={`${props.description}`} />
+        <meta property={`description`} content={`normal comp${props.description}`} />
         <meta property="twitter:title" content={`${props.title}`} />
         <meta property={`twitter:card`} content={`${props.title}`} />
         <meta property={`twitter:site`} content="@gojektech" />
         <meta property={`twitter:image`} content="/img/og-img.jpg" />
-        <meta property="twitter:description" content={`${props.title}`} />
-        {/* <meta property="og:title" content={`${props.title}`} key="title" /> */}
-        <meta property="og:title" content={`${props.title}`} />
+        <meta property="twitter:description" content={`${props.description}`} />
+        <meta property="og:title" content={`${props.title}`} key="title" />
         <meta property="og:url" content="https://gojek.io" />
         <meta property="og:image" content="/img/og-img.jpg" />
-        <meta property="og:description" content={`${props.description}`} />
+        <meta property="og:description" content={`og comp${props.description}`} />
         <meta property="og:type" content={'website'} />
         <link rel="shortcut icon" href="/img/favicon.png" type="image/x-icon " />
         <link rel="icon" href="/img/favicon.png" type="image/x-icon" />
