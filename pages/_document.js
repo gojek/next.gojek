5bef2cf2af40d9bd80c978e71d1439d0c86cebd9
Praful Raj H M <praful.raj@zysk.in>
2021-03-04 13:22:29 +0530

    added script source comment

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import favicon from '~/../../static/favicon.ico';
+import favicon from '~/../../static/favicon.png';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -8,10 +8,66 @@ class MyDocument extends Document {
   }
 
   render() {
-    const name = 'Gojek'
+    const name = 'Gojek';
     return (
       <Html lang="en">
         <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: ` MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNNNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMmddyyso++//////++++osyhdmNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNmhyo+///////+++oooooo+++///////+sydNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMNdyo+////+osyhdmNNNNNNMMMNNNNNmdhyo+////++ydNMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMmhs+////oshmNNMMMMMMMMMMMMMMMMMMMMMMMMNNdyo+///+sdNMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMy+///oydNNMMMMMMMMMNNmmdddddddmmmNMMMMMMMMMMNdy+///+yMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMNyshmNMMMMMMMMmdhso++///////////+++sydmNMMMMMMMNmy++sMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMmds++//+osyhdmmmmmmmmdhyoo+//+ohmMMMMMMMNNMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMMMMmyo//+oydmNMMMMMMMMMMMMMMMMMNNmyo+//oymMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMMMd+/oydNMMMMMMMMMMMNNNNNNMMMMMMMMMMNds+//yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMMMMmmMMMMMMMMMNmhysoooooooosydmNMMMMMMMNhhmMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNmho++osyddmmmmdhys++oymNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMmo+oydNMMMMMMMMMMMMMMmho/+hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNdmMMMMMMMMMMMMMMMMMMMMMmyhMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMmysssydMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMs///////oNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh/////////sMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh/////////yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy+////////yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy//////shmMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMs////////+yNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMs////////+///ohMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMs////////dMdy+///ymMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMs////////hMMMMMNdyo++yhdmMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMs////////yMMMMMMMMMMMNdh//dMMMNmdNMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMd/////////hMMMNmdhyysoooo//oso+///yMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMMNdhhhhhhhho/////////ys+////////////////////yMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMNo//////////////////////////////////////////yMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMs////////////////////////////////////////ohhmMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMmhyo/////////////////////////////////////oy///dMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMdo+//////////////////////////////////////+hNMh///dNmmddmmNMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMmo/////////////////////////////////////hmmmNMMMMh///+//////+oymMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMy///////oydmmy//+///////////////////////mMMMMMMMh+////+yyyys+///odMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMNo/////smMMMNN+/+Nms/////////////////////NMMMMMNo///so//dMMMMNms///sNMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMNNN////hMMMho++//yMMMy///////////////////:MMMMMMo///hMNo//sydNMMMd///sMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMmsso////ssso///////syhy///////////////////oMMMMMd///sMMN+/////+mMMMh///mMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMNNd///+mmm+///////oddh////sssssssssssssydNMMMMMh///hMMh///////yMMMd///dMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMM+///hMMmo/////omMMy///oMMMMMMMMMMMMmmmmmmmmMm///oMMNs+////oNMMMs///NMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMd////yNMMmdhdmMMms////mMMMMMMMMMMMMMMMMMMMMMMs///omMMmdddmMMMNs///yMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMd+////ohdmmmdyo////omMMMMMMMMMMNNNNNNNNNNNNMMy////sdNNNNNNds+///hMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMNh+/////////////ohMMMMMMMMNNNNNNNmmmmmmmmmNNMms+////++++////+smMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMmhso++/++oshmMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNdyo+///++oydNMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMMMMNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+          If you're looking, we're hiring :) - work+source@go-jek.com`,
+            }}
+          />
           <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
           <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
@@ -21,7 +77,6 @@ class MyDocument extends Document {
             integrity="sha384-aOkxzJ5uQz7WBObEZcHvV5JvRW3TUc2rNPA7pe3AwnsUohiw1Vj2Rgx2KSOkF5+h"
             crossorigin="anonymous"
           />
-
           {/* Allow installing the app to the homescreen */}
           <meta name="application-name" content={name} />
           <meta name="apple-mobile-web-app-capable" content="yes" />
@@ -30,13 +85,10 @@ class MyDocument extends Document {
           {/* <meta name="msapplication-config" content="/icons/icon-512x512.png" /> */}
           <meta name="msapplication-TileColor" content="#ffffff" />
           <meta name="msapplication-tap-highlight" content="no" />
-
           {/* <link rel="apple-touch-icon" sizes="180x180" href="/icons/icon-512x512.png" /> */}
           <link rel="shortcut icon" type="image/x-icon" href={favicon} />
-
           {/* Adding Manifest json */}
           <link rel="manifest" href="/manifest.json" />
-
           {/* Social media tags */}
           {/* <meta name="description" content={description} />
           <meta name="twitter:title" content={title} />
@@ -47,7 +99,6 @@ class MyDocument extends Document {
           <meta property="og:site_name" content={name} />
           <meta property="og:url" content={url} /> */}
           {/* <meta property="og:image" content="/icons/icon-152x152.png"></meta> */}
-
           {/* PWA primary color */}
           <meta name="theme-color" content={'#f7ce55'} />
         </Head>
